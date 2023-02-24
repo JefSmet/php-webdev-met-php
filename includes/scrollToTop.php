963b6b13d883cd853df0e332f4e98b55e9fb5178
Jef Smet <jef.smet@student.kdg.be>
2023-02-24 11:47:49 +0100

    Scroll to top more readable

--- includes/scrollToTop.php
+++ includes/scrollToTop.php
@@ -1,16 +1,35 @@
 <!-- Scroll To Top -->
-<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-arrow-circle-up"></span></div>
 
-<script src="/resources/js/jquery.js"></script>
-<script src="/resources/js/popper.min.js"></script>
-<script src="/resources/js/bootstrap.min.js"></script>
-<script src="/resources/js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="/resources/js/jquery.fancybox.js"></script>
-<script src="/resources/js/appear.js"></script>
-<script src="/resources/js/owl.js"></script>
-<script src="/resources/js/wow.js"></script>
-<script src="/resources/js/parallax.min.js"></script>
-<script src="/resources/js/tilt.jquery.min.js"></script>
-<script src="/resources/js/jquery.paroller.min.js"></script>
-<script src="/resources/js/jquery-ui.js"></script>
-<script src="/resources/js/script.js"></script>
+    <div class="scroll-to-top scroll-to-target" data-target="html">
+
+        <span class="fa fa-arrow-circle-up"></span>
+
+    </div>
+
+    <script src="/resources/js/jquery.js"></script>
+
+    <script src="/resources/js/popper.min.js"></script>
+
+    <script src="/resources/js/bootstrap.min.js"></script>
+
+    <script src="/resources/js/jquery.mCustomScrollbar.concat.min.js"></script>
+
+    <script src="/resources/js/jquery.fancybox.js"></script>
+
+    <script src="/resources/js/appear.js"></script>
+
+    <script src="/resources/js/owl.js"></script>
+
+    <script src="/resources/js/wow.js"></script>
+
+    <script src="/resources/js/parallax.min.js"></script>
+
+    <script src="/resources/js/tilt.jquery.min.js"></script>
+
+    <script src="/resources/js/jquery.paroller.min.js"></script>
+
+    <script src="/resources/js/jquery-ui.js"></script>
+
+    <script src="/resources/js/script.js"></script>
+
+<!-- End Scroll To Top -->
