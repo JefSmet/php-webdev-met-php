1d842b139118def54344ed6a50ae4c15d2d40adb
Jef Smet <jef.smet@student.kdg.be>
2023-02-24 11:51:10 +0100

    Update service.php

--- service.php
+++ service.php
@@ -567,22 +567,7 @@
 </div>
 <!-- End Header Search -->
 
-<!-- Scroll To Top -->
-<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-arrow-circle-up"></span></div>
-
-<script src="/resources/js/jquery.js"></script>
-<script src="/resources/js/popper.min.js"></script>
-<script src="/resources/js/bootstrap.min.js"></script>
-<script src="/resources/js/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="/resources/js/jquery.fancybox.js"></script>
-<script src="/resources/js/appear.js"></script>
-<script src="/resources/js/owl.js"></script>
-<script src="/resources/js/wow.js"></script>
-<script src="/resources/js/parallax.min.js"></script>
-<script src="/resources/js/tilt.jquery.min.js"></script>
-<script src="/resources/js/jquery.paroller.min.js"></script>
-<script src="/resources/js/jquery-ui.js"></script>
-<script src="/resources/js/script.js"></script>
+<?php include "includes\scrollToTop.php";?>
 
 </body>
 </html>
